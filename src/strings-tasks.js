4f62bde0440ffa1f87e017783b173bbc68686ec7
MukhayyoBakhodir <mukhayyobakhodir@gmail.com>
2025-10-28 01:36:43 +0500

    feat: task-21

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -358,8 +358,12 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const words = str.split(' ');
+  const reversedWords = words.map((word) => {
+    return word.split('').reverse().join('');
+  });
+  return reversedWords.join(' ');
 }
 
 /**
