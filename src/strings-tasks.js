94c655e20dfe8c705226166e104fcf7d3371b15b
MukhayyoBakhodir <mukhayyobakhodir@gmail.com>
2025-10-27 23:36:00 +0500

    feat: task-8

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -130,8 +130,11 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (typeof str !== 'string' || typeof times !== 'number' || times < 0) {
+    return '';
+  }
+  return str.repeat(times);
 }
 
 /**
